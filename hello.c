bc9cf25952e9bf0cecd27b25d2029e7c528b4bf8
乙火 <dustin.ljl@alibaba-inc.com>
2024-08-26 15:30:00 +0800

    test

--- hello.c
+++ hello.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 
 int main() {
-    const char* token = getenv("TOKEN"); 
+    const char* token = getenv("token"); 
     if (token) {
         while(1) {
             printf("The token is: %s\n", token);

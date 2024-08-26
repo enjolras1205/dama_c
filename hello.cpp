0e731926e59174d4664737cec80975169f5f76b8
乙火 <dustin.ljl@alibaba-inc.com>
2024-08-26 15:00:23 +0800

    test

--- hello.cpp
+++ hello.cpp
@@ -1,12 +1,9 @@
 #include <iostream>
 #include <cstdlib>
-#include <curl/curl.h>
 
 using namespace std;
 
 int main() {
-    CURL* curl;
-    CURLcode res;
     const char* token = std::getenv("token")
     while(1) {
         cout << "the token is: " << token << endl; 

ebd5a87106b0d8bb9a4a771da5013b8c681ebe63
乙火 <dustin.ljl@alibaba-inc.com>
2024-08-26 15:03:19 +0800

    test

--- Dockerfile
+++ Dockerfile
@@ -3,13 +3,13 @@ FROM kun-registry.cn-shenzhen.cr.aliyuncs.com/kun-build/plat1024-c_cpp:202408221
 
 USER root
 # 创建一个简单的 C 程序
-COPY hello.cpp /app/bin/hello.cpp
+COPY hello.c /app/bin/hello.c
 
 # 设置工作目录
 WORKDIR /app/bin
 
 # 编译 C 程序
-RUN gcc -o hello hello.cpp
+RUN gcc -o hello hello.c
 
 USER admin
 # 运行 C 程序
---
+++ hello.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    const char* token = getenv("TOKEN"); 
+    if (token) {
+        while(1) {
+            printf("The token is: %s\n", token);
+        }
+    } else {
+        printf("Environment variable 'TOKEN' is not set.\n");
+    }
+    return 0;
+}
